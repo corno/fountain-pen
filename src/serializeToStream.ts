b0878f7bf06aba52746c8f752b179afbf1b183af
corno <gitkraken.a@schraverus.com>
2021-02-03 22:09:58 +0100

    added comments

--- src/serializeToStream.ts
+++ src/serializeToStream.ts
@@ -3,6 +3,13 @@ import * as p from "pareto"
 import * as p20 from "pareto-20"
 import { serialize } from "./serialize"
 
+/**
+ *
+ * @param block
+ * @param indentation tabs or spaces?
+ * @param trimEndWhitespace if a line has whitespace at the end, should it be trimmed?
+ * @param newline every chunk of data is a line. The newline that is specified with this parameter will be appended to every line.
+ */
 export function serializeToStream(
     block: Block,
     indentation: string,
